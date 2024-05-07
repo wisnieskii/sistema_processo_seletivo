c311376781740dfca42bec983f4c527afe2b2e55
Amanda Wisnieski <amanda.202310960@unilasalle.edu.br>
2024-05-07 19:33:18 -0400

    Commit Case 1 feat: Adiciona interação com candidatos e decisão de contratação

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan= new Scanner(System.in);
-        double salarioBase=2.000;
+        int salarioBase=2000;
+
+        String[] candidatos={"Lucas", "Carol", "Lucia", "João"," Victória"};
+
+        for (int i=0; i<candidatos.length;i++){
+            System.out.println("Qual o salario pretendido pelo candidato "+candidatos[i]+":");
+            int salarioPretendido=scan.nextInt();
+            if (salarioPretendido<salarioBase){
+                System.out.println("Ligar para o candidato!!");
+            } else if (salarioPretendido== salarioBase) {
+                System.out.println("Fazer uma contraproposta!!");
+            }else {
+                System.out.println("Aguardar a resposta dos demais candidatos!!");
+            }
+
+        }
+        System.out.println("Acabaram os candidatos para essa vaga");
 
 
     }
